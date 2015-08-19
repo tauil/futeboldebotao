9f46f895360c800a5f3c7da051d1ae9838f20cb2
Rafael B. Tauil <rafael@tauil.com.br>
2015-08-18 22:18:36 -0300

    Adds Match scopes and relations with Player

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,8 @@
 class Match < ActiveRecord::Base
+  belongs_to :player, polymorphic: true
+
+  scope :won, -> { |player_id| where(winner_id: player_id) }
+  scope :lost, -> { |player_id| where( Match.arel_table[:winner_id].is_not(player_id).
+                                       and(Match.arel_table[:home_player_id].is(player_id).
+                                           or(Match.arel_table[:visitor_player_id].is(player_id)))) }
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ActiveRecord::Base
+  has_many :matches, as: :home_player
+  has_many :matches, as: :visitor_player
 end
